e90363e17470ac86e905940ca573947a5306c1dd
Thomas Gann <thomasleegann@gmail.com>
2013-04-25 04:17:11 -0400

    Moved TaigaDrawer member functions to TaigaRender.cpp

--- engine/TaigaRender.cpp
+++ engine/TaigaRender.cpp
@@ -75,3 +75,94 @@ int image_height(ALLEGRO_BITMAP* image)
 {
 	return image ? al_get_bitmap_height(image) : 0;
 }
+
+//===========================================
+//Member functions for the TaigaSprite class.
+//===========================================
+TaigaSprite::TaigaSprite()
+{
+	sprite = nullptr;
+	x = y = 0;
+	flipflags = 0;
+}
+
+TaigaSprite::TaigaSprite(ALLEGRO_BITMAP* spr, int xx, int yy, int flags)
+{
+	sprite = spr;
+	x = xx;
+	y = yy;
+	flipflags = flags;
+}
+
+void TaigaSprite::draw()
+{
+	if(sprite)
+		al_draw_bitmap(sprite, x, y, flipflags);
+}
+
+//=============================================
+//Member functions for the TaigaSpriteEx class.
+//=============================================
+TaigaSpriteEx::TaigaSpriteEx()
+{
+	sprite = nullptr;
+	x = y = 0;
+	angle = 0;
+	flipflags = 0;
+	centerx = centery = 0;
+	scalex = scaley = 0;
+}
+
+TaigaSpriteEx::TaigaSpriteEx(ALLEGRO_BITMAP* spr, int xx, int yy, int flags)
+{
+	sprite = spr;
+	x = xx;
+	y = yy;
+	flipflags = flags;
+	angle = 0;
+	centerx = centery = 0;
+	scalex = scaley = 0;
+}
+
+void TaigaSpriteEx::draw()
+{
+	if(sprite)
+	{
+		al_draw_scaled_rotated_bitmap(sprite, centerx, centery, x, y,
+										scalex, scaley, angle, flipflags);
+	}
+}
+
+//==========================================
+//Member functions for the TaigaClear class.
+//==========================================
+TaigaClear::TaigaClear(int rr, int gg, int bb)
+{
+	r = rr;
+	g = gg;
+	b = bb;
+}
+
+void TaigaClear::draw()
+{
+	al_clear_to_color(al_map_rgb(r,g,b));
+}
+
+//=========================================
+//Member functions for the TaigaText class.
+//=========================================
+TaigaText::TaigaText(std::string txt, ALLEGRO_FONT* fnt, int xx, int yy, ALLEGRO_COLOR clr, int flgs)
+{
+	text = txt;
+	font = fnt;
+	x = xx;
+	y = yy;
+	flags = flgs;
+	color = clr;
+}
+
+void TaigaText::draw()
+{
+	if(font)
+		al_draw_text(font, color, x, y, flags, text.data());
+}
--- engine/TaigaRender.h
+++ engine/TaigaRender.h
@@ -46,26 +46,9 @@ class TaigaSprite : public TaigaDrawer
 		ALLEGRO_BITMAP* sprite;
 		int flipflags;
 
-		TaigaSprite()
-		{
-			sprite = nullptr;
-			x = y = 0;
-			flipflags = 0;
-		}
-
-		TaigaSprite(ALLEGRO_BITMAP* spr, int xx, int yy, int flags = 0)
-		{
-			sprite = spr;
-			x = xx;
-			y = yy;
-			flipflags = flags;
-		}
-
-		virtual void draw()
-		{
-			if(sprite)
-				al_draw_bitmap(sprite, x, y, flipflags);
-		}
+		TaigaSprite();
+		TaigaSprite(ALLEGRO_BITMAP* spr, int xx, int yy, int flags = 0);
+		virtual void draw();
 };
 
 class TaigaSpriteEx : public TaigaDrawer
@@ -77,35 +60,9 @@ class TaigaSpriteEx : public TaigaDrawer
 		float centerx, centery;
 		float scalex, scaley;
 
-		TaigaSpriteEx()
-		{
-			sprite = nullptr;
-			x = y = 0;
-			angle = 0;
-			flipflags = 0;
-			centerx = centery = 0;
-			scalex = scaley = 0;
-		}
-
-		TaigaSpriteEx(ALLEGRO_BITMAP* spr, int xx, int yy, int flags = 0)
-		{
-			sprite = spr;
-			x = xx;
-			y = yy;
-			flipflags = flags;
-			angle = 0;
-			centerx = centery = 0;
-			scalex = scaley = 0;
-		}
-
-		virtual void draw()
-		{
-			if(sprite)
-			{
-				al_draw_scaled_rotated_bitmap(sprite, centerx, centery,
-												x, y, scalex, scaley, angle, flipflags);
-			}
-		}
+		TaigaSpriteEx();
+		TaigaSpriteEx(ALLEGRO_BITMAP* spr, int xx, int yy, int flags = 0);
+		virtual void draw();
 };
 
 class TaigaClear : public TaigaDrawer
@@ -113,17 +70,8 @@ class TaigaClear : public TaigaDrawer
 	public:
 		int r, g, b;
 
-		TaigaClear(int rr, int gg, int bb)
-		{
-			r = rr;
-			g = gg;
-			b = bb;
-		}
-
-		virtual void draw()
-		{
-			al_clear_to_color(al_map_rgb(r,g,b));
-		}
+		TaigaClear(int rr, int gg, int bb);
+		virtual void draw();
 };
 
 class TaigaText : public TaigaDrawer
@@ -134,21 +82,8 @@ class TaigaText : public TaigaDrawer
 		int flags;
 		ALLEGRO_COLOR color;
 
-		TaigaText(std::string txt, ALLEGRO_FONT* fnt, int xx, int yy, ALLEGRO_COLOR clr, int flgs)
-		{
-			text = txt;
-			font = fnt;
-			x = xx;
-			y = yy;
-			flags = flgs;
-			color = clr;
-		}
-
-		virtual void draw()
-		{
-			if(font)
-				al_draw_text(font, color, x, y, flags, text.data());
-		}
+		TaigaText(std::string txt, ALLEGRO_FONT* fnt, int xx, int yy, ALLEGRO_COLOR clr, int flgs);
+		virtual void draw();
 };
 
 int image_height(ALLEGRO_BITMAP* image);
